6a344706b884d2fd6b950ba9e39cdb1efe980abb
tomekgasowski <tomekgasowski93@gmail.com>
2023-07-20 23:19:19 +0200

    hamburger menu button and navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import '../scss/main.scss';
 
 
-console.log('HELLO 🚀')
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation')
+
+const handleClick = () => {
+    hamburger.classList.toggle('hamburger--active');
+    navigation.classList.toggle('navigation--active');
+}
+
+hamburger.addEventListener('click', handleClick);
